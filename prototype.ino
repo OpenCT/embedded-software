79226ea655e0c0c1bf7bf4d4b56045264809cab5
Benjamin Landers <benjaminrlanders@gmail.com>
2015-03-21 22:33:27 -0700

    changed distrobution of work

--- prototype.ino
+++ prototype.ino
@@ -1,99 +1,44 @@
-byte in[1024];
-byte inIndex,key;
-
+byte buffer[16];
+byte bufI,length;
+boolean bufferFull, running, cancel;
 void setup() {
-  // initialize serial:
   Serial.begin(9600);
-
 }
+
 void loop() {
-  // if there's any serial available, read it:
-  while (Serial.available() > 0) {
-    
-    inIndex = (inIndex+1)%1024;
-    in[inIndex] = Serial.read();
-    
-    if(in[inIndex] != ' ')
-    {
-      byte command = str2command(key,inIndex);
-      key = inIndex + 1;
-      switch(command){
-        case 0:
-          config();
-          break;
-        case 1:
-          debug();
-          break;
-        case 2:
-          load();
-          break;
-        case 3:
-          manual();
-          break;
-        case 4:
-          pause();
-          break;
-        case 5:
-          resume();
-          break;
-        case 6:
-          start();
-          break;
-        case 7:
-          status();
-          break;
-        case 8:
-          stop();
-          break;
-        case 9:
-          test();
-          break;
-        case 10:
-          version();
-          break;
+  recieve();
+  runCommands();
+}
+//Commands and fills a one command buffer
+void recieve(){
+  while(Serial.available()&&!bufferFull){
+    if(bufI = 1){
+      length = buffer[bufI]&15; //4 bits on right
+      //4 left bits will be extra if we need them
+      if(buffer[bufI]>>15){
+        cancel = true;
       }
+    }else{
+      buffer[bufI++] = Serial.read();
     }
-
- }
+    if(length==0){
+      if(running){
+        bufferFull = true;
+      }else{
+        setCommand();
+        bufI = 0;
+      }
+    }else{
+      length--;
+    }
+  }
+}
+//called once per command sent
+void setCommand(){
+  //set flags and shit
 }
 
+//called repeatedly
+void runCommands(){
 
-byte str2command(byte start, byte end){
-  //config, debug, load	, manual, pause, resume, start, status, stop, test, version
-  
-  return 0; //should return index of the command or -1 for non-existant
 }
-void config(void){
-  
-}
-void debug(void){
-  
-}
-void load(void){
-  
-}
-void manual(void){
-  
-}
-void pause(void){
-  
-}
-void resume(void){
-  
-}
-void start(void){
-  
-}
-void status(void){
-  
-}
-void stop(void){
-  
-}
-void test(void){
-  //run test suite
-}
-void version(void){
-  Serial.println("0.0.1");
-}
-
