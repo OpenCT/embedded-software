e58179ad8d97ea66b25f08efa33079e3abe87faa
Benjamin Landers <benjaminrlanders@gmail.com>
2015-04-12 16:17:36 -0700

    gcode translation working + extendable

--- GCode.go
+++ GCode.go
@@ -1,81 +1,129 @@
+package main
+
 import (
-        "strconv"
-        "strings"
-       )
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 type Scanner struct {
-	x, y, units, step float64
-	relative          bool
+	x, y, units, xStep, yStep float64
+	relative                  bool
 }
 
-func (context *Scanner) execute(line string) []byte {
-	out := make([]byte, 2)
+var filter map[string]func(string) (byte, byte, bool)
+var context Scanner
+
+func setup() {
+	filter = make(map[string]func(string) (byte, byte, bool))
+	filter["X"] = func(str string) (byte, byte, bool) {
+		tmp, error := strconv.ParseFloat(str, 64)
+		if error != nil {
+			panic("not a number")
+		}
+		tmp *= context.units * context.xStep
+		if context.relative {
+			context.x += tmp
+		} else {
+			context.x = tmp
+		}
+		return uint8(context.x / 256), uint8(context.x), true
+	}
+	filter["Y"] = func(str string) (byte, byte, bool) {
+		tmp, error := strconv.ParseFloat(str, 64)
+		if error != nil {
+			panic("not a number")
+		}
+		tmp *= context.yStep
+		if context.relative {
+			context.y += tmp
+		} else {
+			context.y = tmp
+		}
+		return uint8(context.y / 256), uint8(context.y), true
+	}
+	filter["S"] = func(str string) (byte, byte, bool) {
+		tmp, error := strconv.ParseInt(str, 10, 16)
+		if error != nil {
+			panic("not a number")
+		}
+		return uint8(tmp / 256), uint8(tmp), true
+	}
+	filter["M"] = filter["S"]
+
+	context.xStep = 100
+	context.yStep = 10
+	context.units = 1
+}
+func main() {
+	setup()
+
+	fmt.Println(execute("G0 X1.4 Y1.2123")) //move to 1.4 1.2123
+	fmt.Println(execute("G91"))             //set relative
+	fmt.Println(execute("G0 X1.4"))         //move to 2.8 1.2123
+}
+func execute(line string) []byte {
 	tokens := strings.Split(line, " ")
 	switch tokens[0] {
-	case "G0":
-	case "G1":
-		cordinates := handleArgs(tokens[1:], "X", "Y")
-		flag := 0
-		if cordinates[0] != "" {
-			tmp, error := strconv.ParseFloat(cordinates[0], 64)
-			if error != nil {
-				panic("not a number")
-			}
-			tmp *= context.units * context.step
-			if context.relative {
-				context.x += tmp
-			} else {
-				context.x = tmp
-			}
-			//checks here
-			out = append(out, uint8(context.x/256), uint8(context.x))
-			flag |= 1
-		}
-		if cordinates[1] != "" {
-			tmp, error := strconv.ParseFloat(cordinates[1], 64)
-			if error != nil {
-				panic("not a number")
-			}
-			tmp *= context.step
-			if context.relative {
-				context.y += tmp
-			} else {
-				context.y = tmp
-			}
-			//checks here
-			out = append(out, uint8(context.y/256), uint8(context.y))
-			flag |= 2
-		}
-		out[1] = byte(flag<<4 + 1) //0yx + 0001 <- command code
-		out[0] = byte(len(out) - 1)
+	case "G0", "G1":
+		return compile([]string{"Y", "X"}, 1, tokens, filter)
 	case "G4":
+		return compile([]string{"S", "M"}, 3, tokens, filter)
 	case "G20":
-		context.setIn()
-		return nil
+		setScale(2.6)
 	case "G21":
-		context.setMM()
-		return nil
-	defualt:
+		setScale(1.0)
+	case "G90":
+		setRelative(false)
+	case "G91":
+		setRelative(true)
+	default:
 		panic("Invalid command")
 	}
 	return nil
 }
 
-func (context *Scanner) setMM() {
-	context.units = 1.0
+func setScale(scale float64) {
+	context.units = scale
 }
-func (context *Scanner) setIn() {
-	context.units = 2.6
+func setRelative(relative bool) {
+	context.relative = relative
 }
 
-func handleArgs(tokens []string, types ...string) []string {
-	out := make([]string, len(types))
+func compile(flags []string, code byte, tokens []string, filter map[string]func(string) (byte, byte, bool)) []byte {
+	args := Parse(tokens, flags)
+	out := make([]byte, 2)
+
+	for i, arg := range args {
+		first, second, set := filter[i](arg)
+		if set {
+			out = append(out, first, second)
+		}
+		out[1] += 1 << uint(IndexOf(i, flags))
+	}
+
+	out[0] = byte(len(out) - 1)
+
+	out[1] = out[1]<<4 + code
+
+	return out
+}
+func Parse(tokens []string, flags []string) map[string]string {
+	out := make(map[string]string)
 	for _, token := range tokens {
-		for i, prefix := range types {
-			if strings.HasPrefix(token, prefix) {
-				out[i] = strings.TrimPrefix(token, prefix)
+		for _, flag := range flags {
+			if strings.HasPrefix(token, flag) {
+				out[flag] = strings.TrimPrefix(token, flag)
 			}
 		}
 	}
 	return out
+}
+func IndexOf(elem string, array []string) int {
+	for i, el := range array {
+		if el == elem {
+			return i
+		}
+	}
+	return -1
 }
